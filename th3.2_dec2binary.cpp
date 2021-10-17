d689bf0e48b25901a21d95c543d4cadd7451b33c
Le Phuong Trung <30748832+AlexHoop140@users.noreply.github.com>
2021-10-17 15:59:59 +0000

    rename the files, delete some executive files

---
+++ th3.2_dec2binary.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "th3.1_stack_array.cpp"
+using namespace std;
+
+int main(){
+    int n = 123;
+    int x;
+    STACK s;
+    initStack(s);
+    // while (n > 0){
+    //     NODE *p = getNode(n % 2);
+    //     push(s, p);
+    //     n /= 2;
+    // }
+
+    while(n > 0){
+      elementType x = n % 2; 
+      push(s, x);
+      n /= 2;
+    }
+
+
+	
+    printStack(s);
+	
+}
