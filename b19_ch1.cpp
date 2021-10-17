2520a67572729a0efc8ff3264127e770f662f7e0
Le Phuong Trung <30748832+AlexHoop140@users.noreply.github.com>
2021-10-17 19:19:52 +0700

    Full version
    Not edited yet

---
+++ b19_ch1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <fstream>
+// #include <sstream>
+#define MaxLength 10;
+
+using namespace std;
+
+typedef string elementType;
+
+typedef struct{
+    elementType elements[10];
+    int front, rear;
+} Queue;
+
+void init(Queue &q){
+    q.front = -1;
+    q.rear = -1; 
+}
+
+bool isEmpty(Queue q){
+    return q.front == -1;
+}
+
+bool isFull(Queue q){
+    // return ((q.rear - q.front + 1) % 10) == 0;
+    return (q.rear - q.front + 1) == 10;
+}
+
+void dequeue(Queue &q){
+    if(!isEmpty(q)){
+        // if(q.front == q.rear) init(q);
+        // else q.front = (q.front + 1) % MaxLength;
+        q.front = q.front + 1;
+        if(q.front > q.rear) init(q);
+    }
+    else 
+        cout << "[ERROR] Empty Queue!" << endl;
+}
+
+void enqueue(elementType row, Queue &q){
+    if(!isFull(q)){
+        if(isEmpty(q)) 
+            q.front = 0;
+        // q.rear = (q.rear + 1) % MaxLength;
+        // q.rear++;
+        // q.elements[q.rear] = row;
+        if(q.rear == 10 - 1){
+            for(int i = q.front; i <= q.rear; i++)
+                q.elements[i-q.front] = q.elements[i];
+        
+            q.rear = 10 - q.front - 1;
+            q.front = 0;
+        }
+        q.rear = q.rear + 1;
+        q.elements[q.rear] = row; 
+    }
+    else
+        cout << "[ERROR] Full Queue!" << endl;
+}
+/*
+void buffer(ifstream &fileIn, Queue &q){
+    fileIn.open("input.txt", ios_base::in);
+
+    if(!fileIn){
+        cout << "Khong tim thay tep tin" << endl;
+        exit(0);
+    };
+    string str;
+    while(getline(fileIn, str, '\n')){
+        if(!isFull(q)){
+            enqueue(str, q);
+        }
+        else{
+            cout << "In tu hang......" << endl;
+            while(!isEmpty(q)){
+                // elementType str2 = q.elements[q.front];
+                cout << q.elements[q.front] << endl;
+                dequeue(q);
+            }
+        }
+
+        enqueue(str, q);
+    }
+
+    // if(!isEmpty(q)){
+        cout << "In cac noi dung con lai trong hang doi...." << endl;
+        while(!isEmpty(q)){
+            elementType str3 = q.elements[q.front];
+            cout << str3 << endl;
+            dequeue(q);
+        }
+    // }
+    fileIn.close();
+}
+*/
+
+
+int main(){
+    ifstream fileIn;
+    Queue q;
+    init(q);
+    // buffer(fileIn, q);
+
+    fileIn.open("input.txt", ios_base::in);
+
+    if(!fileIn){
+        cout << "Khong tim thay tep tin" << endl;
+        exit(0);
+    };
+    string str;
+    while(getline(fileIn, str)){
+        if(!isFull(q)){
+            enqueue(str, q);
+        }
+        else{
+            cout << "In tu hang......" << endl;
+            while(!isEmpty(q)){
+                // elementType str2 = q.elements[q.front];
+                cout << q.elements[q.front] << endl;
+                dequeue(q);
+            }
+            enqueue(str, q);
+        }
+
+        // enqueue(str, q);
+    }
+
+    // if(!isEmpty(q)){
+        cout << "In cac noi dung con lai trong hang doi...." << endl;
+        while(!isEmpty(q)){
+            elementType str3 = q.elements[q.front];
+            cout << str3 << endl;
+            dequeue(q);
+        }
+    // }
+    fileIn.close();
+
+}
---
+++ th18.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+
+/*
+Nhập danh sách stack chứa các số nguyên, sau đó xuất ra màn hình các giá trị số nguyên đó
+
+*/
+
+// khai báo cấu trúc của 1 cái node trong stack
+struct node
+{
+	int data; // stack đang chứa các số nguyên
+	struct node *pNext; // con trỏ liên kết giữa các node với nhau
+};
+typedef struct node NODE;
+
+// khai báo cấu trúc của stack
+struct stack
+{
+	NODE *pTop; // dùng con trỏ đầu để quản lí stack
+};
+typedef struct stack STACK;
+
+// hàm khởi tạo stack
+void KhoiTaoStack(STACK &s)
+{
+	s.pTop = NULL;
+}
+
+// hàm khởi tạo 1 cái node
+NODE *KhoiTaoNode(int x)
+{
+	NODE *p = new NODE();
+	if (p == NULL)
+	{
+		cout << "\nKhong du bo nho de cap phat !!!";
+		return NULL;
+	}
+	p->data = x; // thêm giá trị của biến x vào trong data của cái node
+	p->pNext = NULL;
+	return p;
+}
+
+// =============== KIỂM TRA STACK CÓ RỖNG HAY KHÔNG ===============
+bool IsEmpty(STACK s)
+{
+	// nếu danh sách rỗng
+	if (s.pTop == NULL)
+	{
+		return true;
+	}
+	return false;// danh sách có phần tử
+}
+
+// =============== THÊM 1 PHẦN TỬ VÀO ĐẦU STACK- LIFO ===============
+bool Push(STACK &s, NODE *p)
+{
+	if (p == NULL)
+	{
+		return false;
+	}
+
+	// nếu danh sách rỗng
+	if (IsEmpty(s) == true)
+	{
+		s.pTop = p; // node p chính là node đầu stack
+	}
+	else // danh sách đã có phần tử
+	{
+		p->pNext = s.pTop; // cho con trỏ của node p trỏ đến node đầu danh sách <=> tạo kiên kết
+		s.pTop = p; // cập nhật lại node đầu
+	}
+	return true;
+}
+
+// =============== LẤY PHẦN TỬ ĐẦU STACK VÀ HỦY NÓ ĐI - LIFO ===============
+bool Pop(STACK &s, int &x) // x chính là giá trị cần lấy trong node
+{
+	// nếu danh sách rỗng
+	if (IsEmpty(s) == true)
+	{
+		return false;
+	}
+	else
+	{
+		NODE *p = s.pTop; // node p chính là node thế mạng để tí nữa chúng ta xóa nó đi
+		x = p->data; // gán giá trị của node đầu stack vào biến x	
+		s.pTop = s.pTop->pNext; // cập nhật node đầu stack là node tiếp theo	
+		delete p; // xóa node đầu stack vừa lấy 
+		
+	}
+	return true;// lấy phần tử đầu stack thành công
+}
+
+// hàm xuất danh sách stack
+void XuatStack(STACK s)
+{
+	while (IsEmpty(s) == false)
+	{
+		int x;
+		Pop(s, x);
+		cout << x << " ";
+	}
+
+	if (IsEmpty(s) == true)
+	{
+		cout << "\nDANH SACH DANG RONG KIA";
+	}
+	else
+	{
+		cout << "\nDANH SACH DANG TON TAI PHAN TU";
+	}
+}
+
+int main(){
+    int n = 123;
+    int x;
+    STACK s;
+    KhoiTaoStack(s);
+    while (n > 0){
+        NODE *p = KhoiTaoNode(n % 2);
+        Push(s, p);
+        n /= 2;
+    }
+	
+    XuatStack(s);
+	
+}
